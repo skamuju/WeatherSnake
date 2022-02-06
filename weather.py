091fa45189f3d0ab528c57ba872217660455d3b7
skamuju <63597158+skamuju@users.noreply.github.com>
2022-02-06 16:50:13 -0500

    yes

--- weather.py
+++ weather.py
@@ -10,8 +10,7 @@ def check_conditions(zipcode):
         f"http://api.openweathermap.org/data/2.5/weather?zip={zipcode},us&appid=bd85bd1838a37384556ace68c693d987")
 
     # return [temp, conditions]
-    temp = round(
-        ((int(dict(weather_call.json())["main"]["temp"]) - 273.15) * 1.8 + 32), 2)
+    temp = int((int(dict(weather_call.json())["main"]["temp"]) - 273.15) * 1.8 + 32)
     # cond = int(dict(weather_call.json())["weather"][0]["id"])
 
     return temp
